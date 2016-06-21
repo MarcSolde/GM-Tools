ac30aa249c30b98f35bf01ddff6bfb1101b6574d
MarcS <soldevilla.marc@gmail.com>
2016-06-21 14:19:35 +0000

    ADD code for seting up values in the DB, missing the connection w/ the ORM

--- presentation/main.rb
+++ presentation/main.rb
@@ -1,10 +1,13 @@
 require_relative '../domain/UseCaseController/CtrlRoller'
 require_relative '../domain/UseCaseController/CtrlNameGen'
 require_relative '../domain/UseCaseController/CtrlTreasureGen'
+require_relative '../domain/Model/setUpDB'
 require 'rubygems'
 require 'data_mapper'
 
 class Main
+  db = SetUpDB.new()
+  db.names()
   puts "Select your option"
   puts "1.- Die roller"
   puts "2.- Name Generator (10 names)"
