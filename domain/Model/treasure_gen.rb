b6d278b4aabb4e401c09e63a2d5feb00ba5597cf
MarcSolde <soldevilla.marc@gmail.com>
2016-10-30 16:19:10 +0100

    cleaned up comments

--- domain/Model/treasure_gen.rb
+++ domain/Model/treasure_gen.rb
@@ -1,28 +1,4 @@
 module TreasureGen
-=begin
-  def self.genMinorGem
-    puts "---Minor gems and Jewelry---"
-    result =  File.readlines("./data/tableGemMinor.txt").sample
-    result2 = result.lines("1d")
-    var1 = result2[1].lines("+")
-    b = false
-    if var1[0].to_s == result2[1].to_s
-      b = true
-    end
-    var2 = result2[1].lines("x") if b == true
-    if b == false
-      var3 = var1[1].lines(" ")
-      die = DieRoller::roll("1d"+var1[0])
-      return "Gem or jewelry worth "+(die.to_i+var3[0].to_i).to_s+" gp"
-    elsif var2[0].to_s != result2[1].to_s
-      die =DieRoller::roll("1d100")
-      return "Gem or jewelry worth "+(die.to_i*10).to_s+" gp"
-    else
-      die = DieRoller::roll("1d6")
-      return "Gem or jewelry worth "+die.to_s+" gp"
-    end
-  end
-=end
   def self.genGem(s)
     puts "---"+s+" gems and Jewelry---"
     result = File.readlines("data/tableGemMinor.txt").sample if s=="Minor"
