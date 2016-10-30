aba76cd00c0fffdb18f3720dda6f169120c19bef
MarcSolde <soldevilla.marc@gmail.com>
2016-10-30 16:20:29 +0100

    ADD initializer and tradeouts work for gp and things like that, need to implement Major G&J and Magic Items Trade-Outs

--- domain/UseCaseController/CtrlTreasureGen.rb
+++ domain/UseCaseController/CtrlTreasureGen.rb
@@ -1,24 +1,41 @@
 require_relative '../Model/treasure_gen'
-
+require_relative '../Model/die_roller'
 
 class CtrlTreasureGen
     def initialize()
+      puts "Set the total XP value of the monsters"
+      @gp = (gets.chomp).to_i*DieRoller::roll("1d3+1")
+      @aryTradeout = Array.new(3, 0)
+      for i in 0..@gp/100
+        ++@aryTradeout[0] if (rand(1..100) <= 10)
+      end
+      for i in 0..@gp/1000
+        ++@aryTradeout[1] if (rand(1..100) <= 10)
+      end
+      for i in 0..@gp/5000
+        ++@aryTradeout[2] if (rand(1..100) <= 10)
+      end
+      aux = 100*@aryTradeout[0]+1000*@aryTradeout[1]+5000*@aryTradeout[2]
+      @gp -=aux if aux < @gp
     end
     def generateTresaure()
         #Ask for info about which kind of treasure the user wants to generate
         #Right now we only have the minor gem generator
-        puts "Gem or jewelry worth "+TreasureGen::genGem("Minor").to_s+" gp"
-        puts "Gem or jewelry worth "+TreasureGen::genGem("Medium").to_s+" gp"
-        #puts TreasureGen::genMediumGem()
+      puts @aryTradeout
+        for i in 1..@aryTradeout[0]
+          unless DieRoller::roll("1d20") == 1
+            puts "Gem or jewelry worth "+TreasureGen::genGem("Minor").to_s+" gp"
+          else
+            puts "Not yet implemented"
+          end
+        end
+        for i in 1..@aryTradeout[1]
+          unless DieRoller::roll("1d20") == 1
+            puts "Gem or jewelry worth "+TreasureGen::genGem("Medium").to_s+" gp"
+          else
+            puts "Not yet implemented"
+          end
+        end
+      puts "Plus "+@gp.to_s+" gp"
     end
 end
-
-=begin
-Code for XP to gen and shit like that
-1.- Introduce XP value earnt by the adventure/PCs
-2.- Calculate, from bigger to smaller treasure, which rolls will be made
-3.- Roll for normal or special loot, for each treasure
-4.- Output the result
-DataMapper.setup(:default, 'postgres://user:password@hostname/database')
-
-=end
